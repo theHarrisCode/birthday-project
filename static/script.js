6617f778b6d9fea4c453da416cc05fbca4f84726
Darren Harris <theharriscode401@gmail.com>
2023-09-27 02:54:44 -0400

    drop down day

--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
 $( ".bodyClass" ).on
+
+
+// Get all date containers
+const dateContainers = document.querySelectorAll('.day-container');
+
+// Add click event listeners to each date container
+dateContainers.forEach(container => {
+    const dateTitle = container.querySelector('.date-title');
+    const detailsContainer = container.querySelector('.details-container');
+
+    // Add click event listener to the date title
+    dateTitle.addEventListener('click', () => {
+        // Toggle the visibility of the details container
+        if (detailsContainer.style.display === 'none' || detailsContainer.style.display === '') {
+            detailsContainer.style.display = 'flex';
+        } else {
+            detailsContainer.style.display = 'none';
+        }
+    });
+});
+
